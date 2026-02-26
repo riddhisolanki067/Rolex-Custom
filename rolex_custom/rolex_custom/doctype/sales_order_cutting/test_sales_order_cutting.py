a1664462453c52386bb72ccd7a37ffcce518506c
Riddhi Solanki <riddhisolanki067@gmail.com>
2026-02-26 15:04:16 +0530

    Sales order cutting

---
+++ rolex_custom/rolex_custom/doctype/sales_order_cutting/test_sales_order_cutting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, q and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSalesOrderCutting(FrappeTestCase):
+	pass
