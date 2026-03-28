5246d3421b5bff89c2c8b351e9df20b4e346532e
Riddhi Solanki <riddhisolanki067@gmail.com>
2026-03-28 11:57:16 +0530

    rate amount

--- rolex_custom/rolex_custom/doctype/so_cutting_form/so_cutting_form.js
+++ rolex_custom/rolex_custom/doctype/so_cutting_form/so_cutting_form.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2026, q and contributors
 // For license information, please see license.txt
+frappe.ui.form.on('Sales Order Item', {
+    qty: function(frm, cdt, cdn) {
+        console.log("qty changed");
+        calculate_amount(frm, cdt, cdn);
+    },
+    rate: function(frm, cdt, cdn) {
+        calculate_amount(frm, cdt, cdn);
+    }
+});
 
-// frappe.ui.form.on("SO Cutting Form", {
-// 	refresh(frm) {
-
-// 	},
-// });
+function calculate_amount(frm, cdt, cdn) {
+    console.log("Calculating amount");
+    let row = frappe.get_doc(cdt, cdn);
+    if(row.qty && row.rate) {
+        console.log("qty and rate are present");
+        let amount = row.qty * row.rate;
+        console.log("Calculated amount: " + amount);
+        frappe.model.set_value(cdt, cdn, 'amount', amount);
+        frm.refresh_field('items');
+    
+ }
+}
